22e5db94b5c73fd181f3efb2f4ce91b46c97be24
Kenneth Johnson <kd123johnson@gmail.com>
2021-11-05 10:11:02 -0600

    Gun Test 1

--- GunAnimationtest/GameScene.swift
+++ GunAnimationtest/GameScene.swift
@@ -8,81 +8,71 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene
+{
+    var gun : SKSpriteNode!
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
-    override func didMove(to view: SKView) {
+    override func didMove(to view: SKView)
+    {
+        creategun()
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
+    override func update(_ currentTime: TimeInterval)
+    {
         // Called before each frame is rendered
     }
+    
+    func creategun()
+    {
+        let gunTexture = SKTexture(imageNamed: "sprite_gun00")
+        gun = SKSpriteNode(texture: gunTexture)
+        gun.position = CGPoint(x: frame.midX, y: frame.midY)
+        
+        addChild(gun)
+        
+        let frame2 = SKTexture(imageNamed: "sprite_gun01")
+        let frame3 = SKTexture(imageNamed: "sprite_gun02")
+        let frame4 = SKTexture(imageNamed: "sprite_gun03")
+        let frame5 = SKTexture(imageNamed: "sprite_gun04")
+        let frame6 = SKTexture(imageNamed: "sprite_gun05")
+        let frame7 = SKTexture(imageNamed: "sprite_gun06")
+        let frame8 = SKTexture(imageNamed: "sprite_gun07")
+        let frame9 = SKTexture(imageNamed: "sprite_gun08")
+        let frame10 = SKTexture(imageNamed: "sprite_gun09")
+        let frame11 = SKTexture(imageNamed: "sprite_gun10")
+        let frame12 = SKTexture(imageNamed: "sprite_gun11")
+        let frame13 = SKTexture(imageNamed: "sprite_gun12")
+        let frame14 = SKTexture(imageNamed: "sprite_gun13")
+        let frame15 = SKTexture(imageNamed: "sprite_gun14")
+        let frame16 = SKTexture(imageNamed: "sprite_gun15")
+        let frame17 = SKTexture(imageNamed: "sprite_gun16")
+        let frame18 = SKTexture(imageNamed: "sprite_gun17")
+        let frame19 = SKTexture(imageNamed: "sprite_gun18")
+        let frame20 = SKTexture(imageNamed: "sprite_gun19")
+        let frame21 = SKTexture(imageNamed: "sprite_gun20")
+        let frame22 = SKTexture(imageNamed: "sprite_gun21")
+        let frame23 = SKTexture(imageNamed: "sprite_gun22")
+        let frame24 = SKTexture(imageNamed: "sprite_gun23")
+        let frame25 = SKTexture(imageNamed: "sprite_gun24")
+        let frame26 = SKTexture(imageNamed: "sprite_gun25")
+        let frame27 = SKTexture(imageNamed: "sprite_gun26")
+        let frame28 = SKTexture(imageNamed: "sprite_gun27")
+        let frame29 = SKTexture(imageNamed: "sprite_gun28")
+        let frame30 = SKTexture(imageNamed: "sprite_gun29")
+        
+        
+        let animation = SKAction.animate(withNormalTextures: [gunTexture, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10, frame11, frame12, frame13, frame14, frame15, frame16, frame17, frame18, frame19, frame20, frame21, frame22, frame23, frame24, frame25, frame26, frame27, frame28, frame29, frame30], timePerFrame: 0.15)
+        
+        gun.run(SKAction.repeatForever(animation))
+    }
+    
 }
