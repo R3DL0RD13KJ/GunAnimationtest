4092ed8daf97349adfa460f25acc02b8947c1789
Kenneth Johnson <kd123johnson@gmail.com>
2021-12-01 10:30:19 -0700

    Gun Animation Test 0.0.0.1

--- GunAnimationtest/GameScene.swift
+++ GunAnimationtest/GameScene.swift
@@ -70,7 +70,7 @@ class GameScene: SKScene
         let frame30 = SKTexture(imageNamed: "sprite_gun29")
         
         
-        let animation = SKAction.animate(withNormalTextures: [gunTexture, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10, frame11, frame12, frame13, frame14, frame15, frame16, frame17, frame18, frame19, frame20, frame21, frame22, frame23, frame24, frame25, frame26, frame27, frame28, frame29, frame30], timePerFrame: 0.15)
+        let animation = SKAction.animate(withNormalTextures: [gunTexture, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10, frame11, frame12, frame13, frame14, frame15, frame16, frame17, frame18, frame19, frame20, frame21, frame22, frame23, frame24, frame25, frame26, frame27, frame28, frame29, frame30], timePerFrame: 0.12)
         
         gun.run(SKAction.repeatForever(animation))
     }
